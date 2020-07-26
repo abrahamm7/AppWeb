8b41811baa445cf6b5d686f0793c5774b42e15a4
Abraham Morillo <abrahmmorillo777@gmail.com>
2020-07-26 11:30:07 -0400

    /

--- AppWeb/Controllers/CategoriesController.cs
+++ AppWeb/Controllers/CategoriesController.cs
@@ -1,6 +1,9 @@
 ﻿using AppWeb.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,9 +20,50 @@ namespace AppWeb.Controllers
         
         public List<Categorie> GetCategories()
         {
-            List<Categorie> categories = new List<Categorie>();
-                
-            return categories;
+            try
+            {               
+                List<Categorie> categories = new List<Categorie>();
+
+                string name = Request["usertxt"];
+                string password = Request["passtxt"];
+                string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                string query = $"SELECT * FROM CategoriaTbl";
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, con))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    Categorie categorie = new Categorie();
+
+                                    if (reader[0] != System.DBNull.Value)
+                                    {
+                                        categorie.idcategorie = Convert.ToInt32(reader[0]);
+                                    }
+                                    if (reader[1] != System.DBNull.Value)
+                                    {
+                                        categorie.categorie = reader[1].ToString();
+                                    }                                   
+                                    categories.Add(categorie);
+                                }
+                            }
+                        }
+                    }
+                    con.Close();
+                    return categories;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
         }
     }
 }
