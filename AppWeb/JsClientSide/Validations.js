8d0e5dcca2ff7bb096273d4e044b87744c290b44
Abraham Morillo <abrahmmorillo777@gmail.com>
2020-08-01 20:59:18 -0400

    Register validation

--- AppWeb/JsClientSide/Validations.js
+++ AppWeb/JsClientSide/Validations.js
@@ -1,7 +1,19 @@
-﻿function RequestLogin() {
+﻿//Validation for Login Page//
+function RequestLogin() {
     var usertxt = document.getElementById("userbox").value;
     var passtxt = document.getElementById("passbox").value;
     if (usertxt.length == 0 || passtxt.length == 0) {
+        window.alert("Empty fields");
+    }
+}
+
+//Validation for Register Page//
+function RequestRegister() {
+    var nametxt = document.getElementById("nametxt").value;
+    var passtxt = document.getElementById("passtxt").value;
+    var usertxt = document.getElementById("usertxt").value;
+    var lastnametxt = document.getElementById("lastnametxt").value;
+    if (usertxt.length == 0 || passtxt.length == 0 || nametxt.length == 0 || lastnametxt.length == 0 ) {
         window.alert("Empty fields");
     }
 }
