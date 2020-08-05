684a957c38ebc0c24c1890d9064005c085acf99e
Abraham Morillo <abrahmmorillo777@gmail.com>
2020-08-04 22:06:51 -0400

    Page for profile

--- AppWeb/Models/User.cs
+++ AppWeb/Models/User.cs
@@ -12,5 +12,6 @@ namespace AppWeb.Models
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Clave { get; set; }
+        public string Rol { get; set; }
     }
 }
