be60ff874486b5a2d5008f204dc50db194081559
Abraham Morillo <abrahmmorillo777@gmail.com>
2020-08-10 12:12:38 -0400

    Comments in loginController

--- AppWeb/Controllers/LoginController.cs
+++ AppWeb/Controllers/LoginController.cs
@@ -27,26 +27,29 @@ namespace AppWeb.Controllers
         {
             try
             {
-                if (user.UserName == null || user.Password == null)
+                if (user.UserName == null || user.Password == null) //If model is empty, return a error message//
                 {
-                    ViewBag.Message = "Empty fields";
+                    ViewBag.Message = "Empty fields"; 
                     return View();
                 }
                 else
                 {
+                    //Search if the user exist in DB//
                     var finduser = Data.GetAllUsers().Where(element => element.UserName == user.UserName && element.Password == user.Password).ToList();
-                    if (finduser.Count != 0 && finduser.First().Role == "ADMINISTRADOR")
+
+                    
+                    if (finduser.Count != 0 && finduser.First().Role == "ADMINISTRADOR") //If user is admin, navigate to page for CRUD operations//
                     {
                         cookie["iduser"] = finduser.First().UserId.ToString();
                         return RedirectToAction("Index", "Categories");
                     }
-                    else if (finduser.Count != 0 && finduser.First().Role == "USUARIO")
+                    else if (finduser.Count != 0 && finduser.First().Role == "USUARIO") //If user is usuario, navigate to page for purchase and view products//
                     {
                         cookie["iduser"] = finduser.First().UserId.ToString();
                         return RedirectToAction("Index", "ViewUsers");
                     }
 
-                    if (finduser.Count == 0 || finduser == null)
+                    if (finduser.Count == 0 || finduser == null) //If list is empty, return a error message//
                     {
                         ViewBag.Message = "User not found";
                         return View();
