530f1d8f0e0a27f06148f6748707004465732626
Abraham Morillo <abrahmmorillo777@gmail.com>
2020-08-10 12:08:42 -0400

    Ajax in login form

--- AppWeb/Controllers/LoginController.cs
+++ AppWeb/Controllers/LoginController.cs
@@ -13,71 +13,55 @@ namespace AppWeb.Controllers
 {
     public class LoginController : Controller
     {
-        IUserModule Data = new UserModule();
+        IUserModule Data = new UserModule();        
         HttpCookie cookie = new HttpCookie("cookie");     
 
         // GET: Login
         public ActionResult Index()
+        {          
+            return View();            
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user) //Login for users//
         {
             try
             {
-                string btnclick = Request["signuser"];               
-                if (btnclick == "Login")
+                if (user.UserName == null || user.Password == null)
                 {
-                    string user = Request["usertxt"];
-                    string password = Request["passtxt"];                
-                    Response.Cookies.Add(cookie); //This coockie is for save the username//
-                   
-                    var finduser = Data.GetAllUsers().Where(element => element.UserName == user && element.Password == password).ToList();                   
-
+                    ViewBag.Message = "Empty fields";
+                    return View();
+                }
+                else
+                {
+                    var finduser = Data.GetAllUsers().Where(element => element.UserName == user.UserName && element.Password == user.Password).ToList();
                     if (finduser.Count != 0 && finduser.First().Role == "ADMINISTRADOR")
                     {
                         cookie["iduser"] = finduser.First().UserId.ToString();
                         return RedirectToAction("Index", "Categories");
                     }
-                    else if(finduser.Count != 0 && finduser.First().Role == "USUARIO")
+                    else if (finduser.Count != 0 && finduser.First().Role == "USUARIO")
                     {
                         cookie["iduser"] = finduser.First().UserId.ToString();
                         return RedirectToAction("Index", "ViewUsers");
                     }
-                    if (finduser.Count == 0)
+
+                    if (finduser.Count == 0 || finduser == null)
                     {
-                        Response.Write($"<script>alert('This user: {user} not found');</script>");
+                        ViewBag.Message = "User not found";
                         return View();
                     }
                 }
+                
+                return View();
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"Error en: {ex.Message}");
-            }           
-            return View();
-            
-        }
-        
-        public ActionResult Login()
-        {
-            string user = Request["usertxt"];
-            string password = Request["passtxt"];
-            Response.Cookies.Add(cookie); //This coockie is for save the username//
-
-            var finduser = Data.GetAllUsers().Where(element => element.UserName == user && element.Password == password).ToList();
-
-            if (finduser.Count != 0 && finduser.First().Role == "ADMINISTRADOR")
-            {
-                cookie["iduser"] = finduser.First().UserId.ToString();
-                return RedirectToAction("Index", "Categories");
-            }
-            else if (finduser.Count != 0 && finduser.First().Role == "USUARIO")
-            {
-                cookie["iduser"] = finduser.First().UserId.ToString();
-                return RedirectToAction("Index", "ViewUsers");
-            }
-            if (finduser.Count == 0)
-            {
+                Debug.WriteLine(ex.Message);
                 return View();
             }
-            return View();
+           
         }
+      
     }
 }
